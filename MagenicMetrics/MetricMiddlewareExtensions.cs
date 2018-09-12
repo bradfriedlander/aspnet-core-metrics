4a35374d4a4711bcdd3f3e850f74bf02dd6d349b
Brad Friedlander <brad@friedlander.biz>
2018-09-12 05:37:54 -0400

    Simplified start up by adding extension method for AddMetrics.

--- MagenicMetrics/MetricMiddlewareExtensions.cs
+++ MagenicMetrics/MetricMiddlewareExtensions.cs
@@ -1,9 +1,27 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace MagenicMetrics
 {
 	public static class MetricMiddlewareExtensions
 	{
+		/// <summary>
+		///     This methods adds all classes used by <see cref="MetricMiddleware" /> to the service collection.
+		/// </summary>
+		/// <param name="services">This is the current services collection.</param>
+		/// <returns>This is the updated services collection.</returns>
+		public static IServiceCollection AddMetrics(this IServiceCollection services)
+		{
+			return services
+				.AddScoped<IMetric, Metric>()
+				.AddScoped<IMetricService, MetricService>();
+		}
+
+		/// <summary>
+		///     This method performs all of the actions needed to add <see cref="MetricMiddleware" /> to the HTTP request processing pipeline.
+		/// </summary>
+		/// <param name="app">This is the current application pipeline.</param>
+		/// <returns>This is the updated HTTP request processing pipeline.</returns>
 		public static IApplicationBuilder UseMetrics(this IApplicationBuilder app)
 		{
 			return app.UseMiddleware<MetricMiddleware>();
