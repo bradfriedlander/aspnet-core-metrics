0e88f9777f4ad79f4ae5e6a2ae9aafb84ffa3b94
Brad Friedlander <brad@friedlander.biz>
2018-09-14 14:55:14 -0400

    Added handling validation errors to demonstrate providing additional metric information. Implemented using an input binding class instead of a single field.

--- demoWebApp/Controllers/AdminController.cs
+++ demoWebApp/Controllers/AdminController.cs
@@ -1,26 +1,65 @@
 ﻿using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using demoWebApp.Models.InputBinding;
 using MagenicMetrics;
 using Microsoft.AspNetCore.Mvc;
 
 namespace demoWebApp.Controllers
 {
-	public class AdminController : Controller
-	{
-		public AdminController(IMetric metric, IMetricService metricService)
-		{
-			_metric = metric;
-			_metricService = metricService;
-		}
+    /// <summary>
+    ///     This controllers is used to display the latest metrics.
+    /// </summary>
+    /// <seealso cref="Controller" />
+    public class AdminController : Controller
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="AdminController" /> class.
+        /// </summary>
+        /// <param name="metric">The metric.</param>
+        /// <param name="metricService">The metric service.</param>
+        public AdminController(IMetric metric, IMetricService metricService)
+        {
+            _metric = metric;
+            _metricService = metricService;
+        }
 
-		private readonly IMetric _metric;
-		private readonly IMetricService _metricService;
+        private readonly IMetric _metric;
+        private readonly IMetricService _metricService;
 
-		public async Task<IActionResult> Index(int count = 25)
-		{
-			var metricList = await _metricService.GetLatest(count);
-			ViewBag.Count = _metric.ResultCount = metricList.Count();
-			return View(metricList);
-		}
-	}
+        /// <summary>
+        ///     Retrieve the latest <see cref="Metric" /> records. <paramref name="adminIndex.Count" /> limits the number to return.
+        /// </summary>
+        /// <param name="adminIndex">This is the input for this action.</param>
+        /// <returns></returns>
+        public async Task<IActionResult> Index(AdminIndex adminIndex)
+        {
+            if (!ModelState.IsValid)
+            {
+                _metric.ResultCount = -1;
+                _metric.ExceptionMessage = GetValidationErrors();
+                return View();
+            }
+            var metricList = await _metricService.GetLatest(adminIndex.Count ?? 25);
+            ViewBag.Count = _metric.ResultCount = metricList.Count();
+            return View(metricList);
+        }
+
+        private string GetValidationErrors()
+        {
+            var validationErrors = new StringBuilder();
+            foreach (var modelState in ModelState.Values)
+            {
+                foreach (var error in modelState.Errors)
+                {
+                    if (validationErrors.Length != 0)
+                    {
+                        validationErrors.Append("; ");
+                    }
+                    validationErrors.Append(error.ErrorMessage);
+                }
+            }
+            return validationErrors.ToString();
+        }
+    }
 }
---
+++ demoWebApp/Models/InputBinding/AdminIndex.cs
@@ -0,0 +1,15 @@
+﻿using DataAnnotationsExtensions;
+
+namespace demoWebApp.Models.InputBinding
+{
+    /// <summary>
+    /// This is the input class for the Index action of the Admin controller.
+    /// </summary>
+    public class AdminIndex
+    {
+        /// <summary>Gets or sets the maximum number of records.</summary>
+        /// <value>The count.</value>
+        [Min(1)]
+        public int? Count { get; set; }
+    }
+}
