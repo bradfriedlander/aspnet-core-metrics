ed0a2dc5038e4a1d00e6e7635190765becb8b18f
Brad Friedlander <brad@friedlander.biz>
2018-09-17 10:32:23 -0400

    Converted to tag helpers for input form. Modified error messages on data annotation attributes.

--- demoWebApp/Models/InputBinding/AdminIndexInput.cs
+++ demoWebApp/Models/InputBinding/AdminIndexInput.cs
@@ -11,20 +11,23 @@ namespace demoWebApp.Models.InputBinding
         ///     Gets or sets the application name filter.
         /// </summary>
         /// <value>This is the application name filter (using T-SQL case-insensitive contains).</value>
+        [Display(Name = "Application Name Filter")]
         public string ApplicationFilter { get; set; }
 
         /// <summary>
         ///     Gets or sets the page number.
         /// </summary>
         /// <value>This is the page number where <see cref="PageSize" /> is the page size.</value>
-        [Range(1, int.MaxValue)]
+        [Range(1, int.MaxValue, ErrorMessage = "Page No must be > 0")]
+        [Display(Name = "Page No")]
         public int PageNumber { get; set; }
 
         /// <summary>
         ///     Gets or sets the maximum number of records to be shown on a page.
         /// </summary>
         /// <value>The count.</value>
-        [Range(1, int.MaxValue)]
+        [Range(1, int.MaxValue, ErrorMessage = "Page Size must be > 0")]
+        [Display(Name = "Page Size")]
         public int PageSize { get; set; }
     }
 }
