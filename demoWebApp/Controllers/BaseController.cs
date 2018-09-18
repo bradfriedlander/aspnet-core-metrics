325c90980db73e211f6e032af829ca1de8c0d1aa
Brad Friedlander <brad@friedlander.biz>
2018-09-18 05:28:40 -0400

    Moved shared part of API routing to base class. Added C# documentation.

--- demoWebApp/Controllers/BaseController.cs
+++ demoWebApp/Controllers/BaseController.cs
@@ -4,15 +4,30 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace demoWebApp.Controllers
 {
+    /// <summary>
+    ///     This is the base class for all controllers.
+    /// </summary>
+    /// <seealso cref="Controller" />
     public class BaseController : Controller
     {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="BaseController" /> class.
+        /// </summary>
+        /// <param name="metric">The metric.</param>
         public BaseController(IMetric metric)
         {
             _metric = metric;
         }
 
+        /// <summary>
+        ///     The is the current <see cref="IMetric" /> instance.
+        /// </summary>
         public readonly IMetric _metric;
 
+        /// <summary>
+        ///     Gets the concatenated validation errors.
+        /// </summary>
+        /// <returns>This is the concatenated validation errors with "|" uses as a separator.</returns>
         protected string GetValidationErrors()
         {
             var validationErrors = new StringBuilder();
@@ -22,7 +37,7 @@ namespace demoWebApp.Controllers
                 {
                     if (validationErrors.Length != 0)
                     {
-                        validationErrors.Append("; ");
+                        validationErrors.Append("| ");
                     }
                     validationErrors.Append(error.ErrorMessage);
                 }
