228df5aab7e62c3d274d299c794a046d41913595
Brad Friedlander <brad@friedlander.biz>
2018-09-18 16:20:34 -0400

    Change attribute to use dependency injection.

--- demoWebApi/Controllers/EnsureDefinitionExistsAttribute.cs
+++ demoWebApi/Controllers/EnsureDefinitionExistsAttribute.cs
@@ -1,53 +1,20 @@
-﻿using demoWebApi.InputModels;
-using demoWebApi.Models;
-using demoWebApi.Services;
+﻿using demoWebApi.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace demoWebApi.Controllers
 {
     /// <summary>
-    ///     This attribute ensure that the record id is valid and that there is a matching <see cref="Definition" /> record.
+    ///     This attribute ensures that the record id is valid and that there is a matching <see cref="Definition" /> record.
     /// </summary>
     /// <seealso cref="ActionFilterAttribute" />
-    public class EnsureDefinitionExistsAttribute : ActionFilterAttribute
+    public class EnsureDefinitionExistsAttribute : TypeFilterAttribute
     {
         /// <summary>
-        ///     This method is executed before the action begins executing.
+        ///     Initializes a new instance of the <see cref="EnsureDefinitionExistsAttribute" /> class.
         /// </summary>
-        /// <param name="context">This is the context for the action.</param>
-        /// <inheritdoc />
-        public override async void OnActionExecuting(ActionExecutingContext context)
+        public EnsureDefinitionExistsAttribute() : base(typeof(EnsureDefinitionExistsFilter))
         {
-            var definitionContext = (ApiContext)context.HttpContext.RequestServices.GetService(typeof(ApiContext));
-            var definitionId = 0;
-            if (context.ActionArguments.Keys.Contains("id"))
-            {
-                definitionId = (int)context.ActionArguments["id"];
-            }
-            else if (context.ActionArguments.Keys.Contains("definition"))
-            {
-                var definitionInput = (DefinitionInput)context.ActionArguments["definition"];
-                if (!definitionInput.Id.HasValue)
-                {
-                    context.Result = new BadRequestResult();
-                }
-                definitionId = (int)definitionInput.Id;
-            }
-            else
-            {
-                context.Result = new BadRequestResult();
-                return;
-            }
-            if (definitionId <= 0)
-            {
-                context.Result = new BadRequestResult();
-                return;
-            }
-            if (!await definitionContext.DoesDefinitionExist(definitionId))
-            {
-                context.Result = new NotFoundResult();
-            }
         }
     }
 }
