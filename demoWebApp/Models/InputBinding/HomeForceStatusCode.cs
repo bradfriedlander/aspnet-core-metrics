dd37adfd51d5d7d85a8b4b2699898ee0c746f86e
Brad Friedlander <brad@friedlander.biz>
2018-09-25 12:04:05 -0400

    Fixed another form to handle get/post. Fixed errors to show in red.

--- demoWebApp/Models/InputBinding/HomeForceStatusCode.cs
+++ demoWebApp/Models/InputBinding/HomeForceStatusCode.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace demoWebApp.Models.InputBinding
 {
+    /// <summary>
+    ///     This is the input and binding model for status code input.
+    /// </summary>
     public class HomeForceStatusCode
     {
+        /// <summary>
+        ///     Gets or sets the status code value.
+        /// </summary>
+        /// <value>This is the status code value.</value>
+        [Range(200, 599)]
         public int Code { get; set; }
     }
 }
