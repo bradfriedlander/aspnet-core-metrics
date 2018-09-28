3d4fd64034088f0b356d1d5b8d1d4e2c6c014f48
Brad Friedlander <brad@friedlander.biz>
2018-09-28 11:24:19 -0400

    Adding implementation of IdentityServer4.

---
+++ sample/IdentityServer/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using IdentityServer.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace IdentityServer.Controllers
+{
+    public class HomeController : Controller
+    {
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+
+        public IActionResult Index() => View();
+    }
+}
