eff8335933e02bc0126a49816b7a6c36d6a8c6b6
Brad Friedlander <brad@friedlander.biz>
2018-11-05 15:24:38 -0500

    Fixed columns size to handle large requests.

---
+++ migrations/demoMigrations/Migrations/20181105201317_EnlaargeRequest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace demoWebApp.Migrations
+{
+    public partial class EnlaargeRequest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RequestPath",
+                table: "Metrics",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Query",
+                table: "Metrics",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 128);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RequestPath",
+                table: "Metrics",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Query",
+                table: "Metrics",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+    }
+}
